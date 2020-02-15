f8b90edd8b9107b9f378c273125b13e8d73ff107
Michał Noszczyk <noszczykmichal@wp.pl>
2020-02-15 22:29:50 +0100

    pobranie wszystkich przycisków

---
+++ js/main.js
@@ -0,0 +1,25 @@
+"use strict";
+
+let display=document.getElementById('display');
+let button9=document.getElementById('button9');
+let button8=document.getElementById('button8');
+let button7=document.getElementById('button7');
+let button6=document.getElementById('button6');
+let button5=document.getElementById('button5');
+let button4=document.getElementById('button4');
+let button3=document.getElementById('button3');
+let button2=document.getElementById('button2');
+let button1=document.getElementById('button1');
+let button0=document.getElementById('button0');
+let buttonDel=document.getElementById('buttonDel');
+let buttonPercent=document.getElementById('buttonPercent');
+let buttonPlus=document.getElementById('buttonPlus');
+let buttonMinus=document.getElementById('buttonMinus');
+let buttonMultiply=document.getElementById('buttonMultiply');
+let buttonDivide=document.getElementById('buttonDivide')
+let buttonDot=document.getElementById('buttonDot');
+let buttonEquals=document.getElementById('');
+
+
+
+
